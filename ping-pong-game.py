e240c690b4ae1352cbcd0929782b8461dc398f6b
emina0c <145202186+emina0c@users.noreply.github.com>
2023-09-23 14:25:34 +0200

    Add files via upload
    prozor

---
+++ ping-pong-game.py
@@ -0,0 +1,19 @@
+from pygame import *
+
+color = (200,255,255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width,win_height))
+window.fill(color)
+
+clock = time.Clock()
+FPS = 60
+game = True
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
